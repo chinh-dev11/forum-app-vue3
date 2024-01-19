114c3e03d22c120596ca2b4eaa9742854dbf51aa
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-19 10:35:17 -0500

    add home route

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from '@/components/PageHome'
+
+const routes = [
+  { name: 'home', path: '/', component: PageHome }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
