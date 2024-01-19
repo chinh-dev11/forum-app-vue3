b56e699241d8601c620220fab7989622ac89da13
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-19 14:18:58 -0500

    extract list items and add styles

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,82 +1,20 @@
-<!-- Composition API -->
-<!-- <script>
-import dataSource from '@/data.json'
-import { reactive } from 'vue'
-export default {
-  setup () {
-    const threads = reactive(dataSource.threads)
-    const posts = reactive(dataSource.posts)
-    const users = reactive(dataSource.users)
-
-    function postById (postId) {
-      return posts.find(p => p.id === postId)
-    }
-    function userById (userId) {
-      return users.find(u => u.id === userId)
-    }
-
-    return {
-      threads,
-      posts,
-      users,
-      postById,
-      userById
-    }
-  }
-}
-</script> -->
-<!-- <script setup>
-import dataSource from '@/data.json'
-import { reactive } from 'vue'
-
-const threads = reactive(dataSource.threads)
-const posts = reactive(dataSource.posts)
-const users = reactive(dataSource.users)
-
-function postById (postId) {
-  return posts.find(p => p.id === postId)
-}
-function userById (userId) {
-  return users.find(u => u.id === userId)
-}
-</script>
- -->
-
-<!-- Options API -->
 <script>
+import ThreadList from './ThreadList.vue'
 import dataSource from '@/data.json'
 
 export default {
+  components: { ThreadList },
   data () {
     return {
-      threads: dataSource.threads,
-      posts: dataSource.posts,
-      users: dataSource.users
-    }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find(p => p.id === postId)
-    },
-    userById (userId) {
-      return this.users.find(u => u.id === userId)
+      threads: dataSource.threads
     }
   }
 }
 </script>
 
 <template>
-<div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <div v-for="postId in thread.posts" :key="postId">
-      <h3>
-        {{ userById(postById(postId).userId).name }}
-      </h3>
-      <p>{{ postById(postId).text }}</p>
-    </div>
-</div>
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads"/>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
