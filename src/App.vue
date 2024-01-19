e3edbdcdaf76895664b27c4b6446643c69039091
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-19 09:53:31 -0500

    add threadlist

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { PageHome }
 }
 </script>
+
+<template>
+  <PageHome />
+</template>
 
 <style>
 #app {
