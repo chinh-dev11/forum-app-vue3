d16899a641e69abfb0eb3d7997b40e692fa0b17c
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-19 16:10:49 -0500

    add route guard and preverve the nonexistent path

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   { name: 'Home', path: '/', component: PageHome },
-  { name: 'ThreadShow', path: '/thread/:id', component: PageThreadShow, props: true },
+  {
+    name: 'ThreadShow',
+    path: '/thread/:id',
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(
+        (t) => t.id === to.params.id
+      )
+
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
+  },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound }
 ]
 
