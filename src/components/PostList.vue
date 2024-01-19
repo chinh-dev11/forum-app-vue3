0280db0c3e3accd69a0e321a1f8cbc6f4f39909d
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-19 17:14:44 -0500

    extract post list

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<script>
+import PostListItem from '@/components/PostListItem.vue'
+
+export default {
+  components: { PostListItem },
+  props: {
+    thread: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="post-list">
+      <PostListItem v-for="postId in thread.posts" :key="postId" :postId="postId" />
+    </div>
+</template>
+
+<style scoped>
+.post-list {
+  margin-top: 20px;
+}
+</style>
