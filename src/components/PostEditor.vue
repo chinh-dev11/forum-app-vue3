3589efd68528e591e61749b00297cb9d5b23b175
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-20 12:57:01 -0500

    extract form editor and add emit event for communicating between components

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'aaaa-' + Math.random() // temp dev value (could also use a package to generate ids). In real world, value should be generated form DB.
+      const post = {
+        // edited: {},
+        publishedAt: Math.floor(Date.now() / 1000), // in secs.
+        text: this.text,
+        // text: this.$refs.textarea.value, // to fix browser error: Assertion failed: Input argument is not an HTMLInputElement.
+        userId: 'Miej9zSGMRZKDvMXzfxjVOyv3RF3',
+        id: postId
+      }
+
+      this.$emit('save', { post })
+
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <form action="" @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input type="text" id="thread_title" class="form-input" name="title" />
+    </div>
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <!-- since 2-way binding uses input event, it causes an error on textarea element 'Assertion failed: Input argument is not an HTMLInputElement' -->
+      <textarea
+        v-model="text"
+        id="thread_content"
+        class="form-input"
+        name="content"
+        rows="8"
+        cols="140"
+      ></textarea>
+      <!-- to fix the error 'Assertion failed: Input argument is not an HTMLInputElement' -->
+      <!-- <textarea
+          :value="text"
+          ref="textarea"
+          id="thread_content"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+        /> -->
+    </div>
+    <div class="btn-group">
+      <button class="btn btn-ghost">Cancel</button>
+      <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
