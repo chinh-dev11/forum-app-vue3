82fb022bf3701868014da70018916b61b83ea20a
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-31 08:26:05 -0500

    setup firestore and upload data

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+// Provided by Firebase > Project settings > General > Your apps
+module.exports = {
+  apiKey: 'AIzaSyCekjUu07YoZe2KWx86JGVtvMIREWuZ-uU',
+  authDomain: 'forum-app-vue3.firebaseapp.com',
+  projectId: 'forum-app-vue3',
+  storageBucket: 'forum-app-vue3.appspot.com',
+  messagingSenderId: '517811724284',
+  appId: '1:517811724284:web:02b8ecb5cc304f50615c79'
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import { createApp } from 'vue'
 import router from '@/router'
 import store from '@/store'
 
+// --- Firebase ---
+// Error: Module not found: Error: Package path . is not exported from package.
+// import firebase from 'firebase'
+// Fix: compat packages are API compatible with namespaced code
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+import firebaseConfig from '@/config/firebase'
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// --- forum App ---
 const forumApp = createApp(App)
 
 forumApp.use(router)
