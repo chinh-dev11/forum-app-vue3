24db382b2fa80e163e22d1701ad3f8742240caad
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-31 19:42:00 -0500

    add firebase config file using env vars

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+// Provided by Firebase > Project settings > General > Your apps
+module.exports = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+}
