562952bb88563acd6a638f30121a19860afd1828
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-02-17 10:44:50 -0500

    add spinner

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <script>
 import TheNavbar from '@/components/TheNavbar.vue'
 import { mapActions } from 'vuex'
+import AppSpinner from './components/AppSpinner.vue'
 
 export default {
   name: 'App',
-  components: { TheNavbar },
+  components: { TheNavbar, AppSpinner },
+  data () {
+    return {
+      showPage: false
+    }
+  },
   methods: {
     ...mapActions(['fetchAuthUser'])
   },
   created () {
     this.fetchAuthUser()
+
+    this.$router.beforeEach(() => {
+      this.showPage = false
+    })
   }
 }
 </script>
 
 <template>
   <TheNavbar />
-  <router-view />
+  <router-view v-show="showPage" @ready="showPage = true" />
+  <AppSpinner v-show="!showPage" />
 </template>
 
 <style>
-@import "assets/style.css";
+@import "@/assets/style.css";
 /* @import "~bootstrap/css/bootstrap.css"; */
 </style>
